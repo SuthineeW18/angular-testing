34325183567902ae6135639177b08978a93a8a70
suthineew <wsuthinee18@gmail.com>
2023-04-29 23:48:22 +0700

    first initial commit of code with unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
+    TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    });
   });
 
   it('should create the app', () => {
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-test'`, () => {
+  it(`should have as title 'welcome to angular-test app!'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.title).toEqual('welcome to angular-test app!');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-test app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('welcome to angular-test app!');
   });
 });
